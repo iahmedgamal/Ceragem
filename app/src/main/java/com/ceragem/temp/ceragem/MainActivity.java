9d9037ea42e5593ecca9762facde9f097e3bde3e
Ahmed Gamal <Ahmad.G.Mustafa@gmail.com>
2016-01-18 23:41:58 +0200

    ParseConifg done

--- app/src/main/java/com/ceragem/temp/ceragem/MainActivity.java
+++ app/src/main/java/com/ceragem/temp/ceragem/MainActivity.java
@@ -2,6 +2,9 @@ package com.ceragem.temp.ceragem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.parse.ParseObject;
+
+import com.parse.Parse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/android/guide#local-datastore
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this);
+
+
+
     }
 }
