e219e46585b120028bd275991816f48bdce470e9
Ahmed Gamal <Ahmad.G.Mustafa@gmail.com>
2016-01-19 06:39:42 +0200

    1-activity_user (not completed) 2-ButterKnife (@Bind) 3-screenOrientation= landscape

--- app/src/main/java/com/ceragem/temp/ceragem/MainActivity.java
+++ app/src/main/java/com/ceragem/temp/ceragem/MainActivity.java
@@ -2,7 +2,10 @@ package com.ceragem.temp.ceragem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.parse.ParseObject;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 import com.parse.Parse;
 
@@ -12,10 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // [Optional] Power your app with Local Datastore. For more info, go to
-        // https://parse.com/docs/android/guide#local-datastore
-        Parse.enableLocalDatastore(this);
+        ButterKnife.bind(this);
 
+        Parse.enableLocalDatastore(this);
         Parse.initialize(this);
 
 
