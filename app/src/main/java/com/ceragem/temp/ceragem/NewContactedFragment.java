828539d4735e799215631895072d22a5a28744a1
ahmed3elshaer <reyo12345o@gmail.com>
2016-01-19 09:13:44 +0200

    new contraced user and new employee forms

---
+++ app/src/main/java/com/ceragem/temp/ceragem/NewContactedFragment.java
@@ -0,0 +1,12 @@
+package com.ceragem.temp.ceragem;
+
+import android.app.Fragment;
+
+/**
+ * Created by root on 19/01/16.
+ */
+public class NewContactedFragment extends Fragment {
+    //TODO
+    //don't forget to set the content to new_contracted_user.xml following the tutorial
+    //http://code.tutsplus.com/tutorials/creating-a-login-screen-using-textinputlayout--cms-24168
+}
---
+++ app/src/main/java/com/ceragem/temp/ceragem/NewEmployeeeFragment.java
@@ -0,0 +1,12 @@
+package com.ceragem.temp.ceragem;
+
+import android.app.Fragment;
+
+/**
+ * Created by root on 19/01/16.
+ */
+public class NewEmployeeeFragment extends Fragment {
+    //TODO
+    //don't forget to set the content to add_new_employee.xml following the tutorial
+    //http://code.tutsplus.com/tutorials/creating-a-login-screen-using-textinputlayout--cms-24168
+}
