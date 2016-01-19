2b2ab3a4a7e786541c02f230ce241014800581b4
ahmed3elshaer <reyo12345o@gmail.com>
2016-01-19 20:14:01 +0200

    fixed scroll bug

--- app/src/main/java/com/ceragem/temp/ceragem/NewContactedFragment.java
+++ app/src/main/java/com/ceragem/temp/ceragem/NewContactedFragment.java
@@ -3,7 +3,7 @@ package com.ceragem.temp.ceragem;
 import android.app.Fragment;
 
 /**
- * Created by root on 19/01/16.
+ * Created by AhmedElshaer on 19/01/16.
  */
 public class NewContactedFragment extends Fragment {
     //TODO
