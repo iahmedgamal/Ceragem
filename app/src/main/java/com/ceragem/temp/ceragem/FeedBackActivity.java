3abe9eab86bc7913ae911aac7324cf50109ba260
Ahmed Gamal <Ahmad.G.Mustafa@gmail.com>
2016-01-24 07:39:46 +0200

    1-contracted_detail.xml 2- employees_detail 3-user_detail

--- app/src/main/java/com/ceragem/temp/ceragem/FeedBackActivity.java
+++ app/src/main/java/com/ceragem/temp/ceragem/FeedBackActivity.java
@@ -16,15 +16,15 @@ public class FeedBackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feedback);
-
-        Spinner spinner = (Spinner) findViewById(R.id.department_spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.department_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+//
+//        Spinner spinner = (Spinner) findViewById(R.id.department_spinner);
+//// Create an ArrayAdapter using the string array and a default spinner layout
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//                R.array.department_array, android.R.layout.simple_spinner_item);
+//// Specify the layout to use when the list of choices appears
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//// Apply the adapter to the spinner
+//        spinner.setAdapter(adapter);
 
         }
     }
